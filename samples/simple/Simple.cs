3a570d5eaea2d2d408a7396f2c84af611a7c8b8f
Andreia Gaita <shana@spoiledcat.net>
2011-03-31 17:54:47 +0100

    little test code

---
+++ samples/simple/Simple.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Soup;
+using Gtk;
+
+namespace Simple {
+
+	public class Simple {
+		public static void Main () {
+			Application.Init ();
+			Message msg = new Message ("GET", "http://www.google.com");
+
+			
+
+		}
+	}
+}
